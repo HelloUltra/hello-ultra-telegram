64d105378276f987044a43358d31be9a4bfbc616
JoongHyun Choi <joonghyun@JoongHyunui-MacBook-Pro.local>
2017-01-22 00:57:43 +0900

    commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# hello-ultra-telegram
-for telegram
---
+++ src/main/java/com/ultra/HelloUltraTelegram1Application.java
@@ -0,0 +1,12 @@
+package com.ultra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloUltraTelegram1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloUltraTelegram1Application.class, args);
+	}
+}
